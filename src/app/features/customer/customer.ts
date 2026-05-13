dd94be5d9515150713064534635d025a78b2a82c
AlexJYad <alexanderjacovlev@gmail.com>
2026-05-13 09:15:36 +0200

    add interface for costumer

--- src/app/features/customer/customer.ts
+++ src/app/features/customer/customer.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ICustomer } from './customer.interface';
 
 @Component({
   selector: 'app-customer',
@@ -7,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './customer.css',
 })
 export class Customer {
-
+  costumers: ICustomer[] = [
+    { name: 'Juan García', avatar: 'https://i.pravatar.cc/150?u=juangarcia' },
+    { name: 'María López', avatar: 'https://i.pravatar.cc/150?u=marialopez' },
+    { name: 'Carlos Rodríguez', avatar: 'https://i.pravatar.cc/150?u=carlosrodriguez' },
+    { name: 'Ana Martínez', avatar: 'https://i.pravatar.cc/150?u=anamartinez' },
+    { name: 'Pedro Sánchez', avatar: 'https://i.pravatar.cc/150?u=pedrosanchez' },
+    { name: 'Laura Fernández', avatar: 'https://i.pravatar.cc/150?u=laurafernandez' },
+    { name: 'Diego Moreno', avatar: 'https://i.pravatar.cc/150?u=diegomoreno' },
+    { name: 'Elena Jiménez', avatar: 'https://i.pravatar.cc/150?u=elenajimenez' },
+    { name: 'Miguel Torres', avatar: 'https://i.pravatar.cc/150?u=migueltorres' },
+    { name: 'Sofia Ruiz', avatar: 'https://i.pravatar.cc/150?u=sofiaruiz' },
+  ];
 }
